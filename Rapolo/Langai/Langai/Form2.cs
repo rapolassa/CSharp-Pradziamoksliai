3f9ca234718f3f449947db606cb9f1c6940f3457
Rapke <rapolas.saulius@gmail.com>
2016-03-16 16:27:30 +0200

    Added files via upload
    Numeriai

---
+++ Rapolo/Langai/Langai/Form2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Langai
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            using (TextWriter sw = new StreamWriter("C:\\numeriai.txt"))
+            {
+                foreach (string item in listBox1.Items)
+                {
+                    sw.WriteLine(item.ToString());
+                }
+            }
+            Process.Start("C:\\numeriai.txt");
+
+
+        }
+    }
+}
